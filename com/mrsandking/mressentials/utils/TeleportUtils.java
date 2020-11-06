6a9633ff260227e9f4db3d91cad9a8fd0747d3db
MrSandKing <56915085+MrSandKing@users.noreply.github.com>
2020-11-06 20:42:34 +0100

    Create TeleportUtils.java

---
+++ com/mrsandking/mressentials/utils/TeleportUtils.java
@@ -0,0 +1,11 @@
+package com.mrsandking.mressentials.utils;
+
+import org.bukkit.entity.Player;
+
+public class TeleportUtils {
+	
+	public static void teleportToPlayer(Player target, Player player) {
+		target.teleport(player);
+	}
+
+}
