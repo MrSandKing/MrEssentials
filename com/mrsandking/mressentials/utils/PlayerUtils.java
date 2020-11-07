2fb83263b0a3c8594b5086ec00417a2122c5f5cb
MrSandKing <56915085+MrSandKing@users.noreply.github.com>
2020-11-07 18:12:05 +0100

    Update PlayerUtils.java

--- com/mrsandking/mressentials/utils/PlayerUtils.java
+++ com/mrsandking/mressentials/utils/PlayerUtils.java
@@ -1,23 +1,52 @@
 package com.mrsandking.mressentials.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class PlayerUtils {
 
-	public static boolean getOnlinePlayer(Player player) {
-		return player.isOnline();	
-	}
-
 	public static void kickPlayer(Player player, String info, String reason, CommandSender sender) {
-		if(getOnlinePlayer(player)) {
-			if(reason.length() >= 2) {
+			
+		if(player != null) {
+			
+			if(!player.isOp()) { 
+				
+				if(reason == null) reason = ChatColor.RED+"-";
+				
 				if(sender != null) {
-					player.kickPlayer(ChatColor.GREEN+info+": "+reason);
+					player.kickPlayer(ChatColor.AQUA+info+": "+ ChatColor.translateAlternateColorCodes('&', reason));
+				}
+				
+			} else {
+				sender.sendMessage(ChatColor.RED+"You can't kick OP!");
+			}
+			
+		} else {
+			sender.sendMessage(ChatColor.RED+"Couldn't find that player!");
+		}
+		
+	}
+	
+	public static void setOP(Player player, boolean op) {
+		
+		for(Player players : Bukkit.getOnlinePlayers()) {
+			if(players.getName().equals(player.getName())) {
+				if(op == true) {
+					player.setOp(true);
+				} else if(op == false) {
+					player.setOp(false);
+				}
+			} else {
+				if(op == true) {
+					player.setOp(true);
+				} else if(op == false) {
+					player.setOp(false);
 				}
 			}
 		}
+		
 	}
 	
 }
